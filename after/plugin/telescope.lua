404421d08dcb14c793d2b3dd145311af47bda178
daltondiaz <d_tecnologia@yahoo.com.br>
2022-12-18 00:45:24 -0300

    config: change preview of telescope to vertical

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,9 @@
+require('telescope').setup({
+    defaults = {
+        layout_strategy = 'vertical'
+    }
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
