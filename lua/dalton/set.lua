31ad2dd28fd4deb7542ae433b8e504248dd4290b
daltondiaz <d_tecnologia@yahoo.com.br>
2022-12-18 01:03:09 -0300

    change: colorcolumn from 80 to 100

--- lua/dalton/set.lua
+++ lua/dalton/set.lua
@@ -7,7 +7,6 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
@@ -28,6 +27,6 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "100"
 
 vim.g.mapleader = " "
