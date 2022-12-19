99864f34804cae4efc0717fca4dc3fbf0230d813
daltondiaz <d_tecnologia@yahoo.com.br>
2022-12-19 11:50:06 -0300

    refactor: add to find buffer in telescope and set of smartcase

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,6 +6,7 @@ require('telescope').setup({
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")});
--- lua/dalton/set.lua
+++ lua/dalton/set.lua
@@ -8,6 +8,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.smartcase = true
 
 vim.opt.wrap = false
 
