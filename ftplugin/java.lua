09587e57ff0c420945b8015bbb97ce6825014ae7
daltondiaz <d_tecnologia@yahoo.com.br>
2023-01-06 20:17:00 -0300

    add config suggested by @willianboman

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,12 @@
 local config = {
-    cmd = {'/home/dalton/.local/share/nvim/mason/bin/jdtls'},
+    cmd = {
+        "jdtls",
+        "--jvm-arg=" .. string.format(
+            "-javaagent:%s",
+            require("mason-registry").get_package("jdtls"):get_install_path() .. "/lombok.jar"
+        ),
+    },
     root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
+-- print(config.cmd[2])
 require('jdtls').start_or_attach(config)
-
