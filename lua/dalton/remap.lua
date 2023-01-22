920780db9f21a036bdcc6d8d9b40ac744333f69d
daltondiaz <d_tecnologia@yahoo.com.br>
2023-01-21 23:54:55 -0300

    add remap to wa and wq

--- lua/dalton/remap.lua
+++ lua/dalton/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>s", vim.cmd.wa)
+vim.keymap.set("n", "<leader>q", vim.cmd.wq)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -7,6 +9,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
