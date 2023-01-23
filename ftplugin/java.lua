e98c23e341ad30bb37ee537e535f309204258d4d
daltondiaz <d_tecnologia@yahoo.com.br>
2023-01-23 11:24:33 -0300

    add pom file to find in root of project on jdtls.setup

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -41,7 +41,7 @@ local config = {
   -- 
   -- This is the default if not provided, you can remove it. Or adjust as needed.
   -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
+  root_dir = require('jdtls.setup').find_root({'pom.xml','.git', 'mvnw', 'gradlew'}),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
