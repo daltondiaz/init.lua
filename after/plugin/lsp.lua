ec2c225b6322d73985b2a4555083fb46ac2c2f8d
daltondiaz <d_tecnologia@yahoo.com.br>
2023-08-10 16:16:14 -0300

    refactor: show diagnostic message

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -41,4 +41,15 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 lsp.setup()
-
+vim.diagnostic.config {
+    underline = true,
+    virtual_text = {
+        prefix = "",
+        severity = nil,
+        source = "if_many",
+        format = nil,
+    },
+    signs = true,
+    severity_sort = true,
+    update_in_insert = false,
+}
