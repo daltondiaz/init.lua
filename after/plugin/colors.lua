9f975e2fa765679f3b9f6ab3ac8c2bd9eeef1035
daltondiaz <d_tecnologia@yahoo.com.br>
2023-08-11 11:33:26 -0300

    refactor: change motion on harpoon 4 item

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -41,9 +41,9 @@ function ColorMyPencils(color)
 	vim.cmd.colorscheme(color)
 
     -- transparent background
-   --[[ vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
  	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
-    vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='none'})
+   --[[ vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='none'})
     vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='none'})
     vim.api.nvim_set_hl(0, 'TelescopePromptTitle', {bg='none'})
     vim.api.nvim_set_hl(0, 'TelescopePromptBorder', {bg='none'})
