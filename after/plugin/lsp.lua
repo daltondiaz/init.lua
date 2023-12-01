883e75acad9de517cceb90335d5518e6041f08dd
Dalton <d_tecnologia@yahoo.com.br>
2023-12-01 14:50:11 -0300

    feat: update lsp of cmp actions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 local lsp = require('lsp-zero')
-
+local cmp = require('cmp')
 lsp.preset('recommended')
 
 lsp.on_attach(function(client, bufnr)
@@ -25,13 +25,23 @@ require('mason-lspconfig').setup({
 })
 
 lsp.setup_servers({'tsserver', 'eslint'})
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-o>'] = cmp.mapping.complete(),
-    ['<cr>'] = cmp.mapping.confirm({ select = true }),
+local cmp_action = lsp.cmp_action()
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+
+        -- Navigate between snippet placeholder
+        --   ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        --  ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- Scroll up and down in the completion documentation
+        --     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        --    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
 })
 
 vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
