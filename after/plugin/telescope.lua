efa0dc86fcfe5812126093bda81093f840ebb56a
daltondiaz <d_tecnologia@yahoo.com.br>
2024-03-08 16:05:28 -0300

    refactor: change in treesitter

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,6 +7,7 @@ require('telescope').setup({
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
+-- local extension = require('telescope.extension')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -17,7 +18,6 @@ vim.keymap.set('n', '<C-l>', builtin.git_commits, {})
 vim.keymap.set('n', '<C-s>', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fw', builtin.diagnostics, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+vim.keymap.set('n', '<leader>fs', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-
