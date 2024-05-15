403c9516857dd17ba1cb05a0a8b517df1786bb07
daltondiaz <d_tecnologia@yahoo.com.br>
2024-05-15 10:08:47 -0300

    change config of harpoon to work with kinesis

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,13 +6,17 @@ harpoon:setup({
     }
 })
 
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>",function() harpoon.ui:toggle_quick_menu(harpoon:list())end)
 
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-r>", function ()harpoon:list():remove()end)
+
+-- I do that because my kinesis querty, btw my leader key is space 
+vim.keymap.set("n", "<leader>6", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>7", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>8", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>9", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>0", function() harpoon:list():select(5) end)
 
 -- vim.keymap.set("n", "<leader>5", function() harpoon:list():replace(1) end)
 
