97da9007bbb135fb619e179024ae6b2b010aeecc
daltondiaz <d_tecnologia@yahoo.com.br>
2024-05-25 12:14:21 -0300

    remap close oil.nvim

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -9,3 +9,6 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Open parent directory in floating window
 vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+
+-- Close the window
+vim.keymap.set("n", "<ESC>", require("oil").close)
